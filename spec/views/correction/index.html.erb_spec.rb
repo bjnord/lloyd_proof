1d47e9a4e3f64a9d9b2fdc8b8ddb76d3e3e0e620
Brent J. Nordquist <brent@nordist.net>
2012-09-29 08:32:06 -0500

    Simple Correction index view

---
+++ spec/views/correction/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "correction/index.html.erb" do
+  before(:all) do
+    @corrections = FactoryGirl.build_list(:correction, 2)
+  end
+
+  it "renders all corrections" do
+    render
+    @corrections.each do |correction|
+      rendered.should contain(correction.current)
+    end
+  end
+end
