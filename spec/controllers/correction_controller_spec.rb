a57ac8b85309cfec569529cec2701196ffb41b4a
Brent J. Nordquist <brent@nordist.net>
2012-09-30 18:26:22 -0500

    RF: Consistent use of symbols, quoted strings

--- spec/controllers/correction_controller_spec.rb
+++ spec/controllers/correction_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe CorrectionController do
 
-  describe "index action" do
+  describe 'index action' do
     it 'returns HTTP success' do
-      get 'index'
+      get :index
       response.should be_success
     end
 
@@ -12,7 +12,7 @@ describe CorrectionController do
       FactoryGirl.create(:correction)
       popeye = 'I yam what I yam.'
       FactoryGirl.create(:correction, :current => popeye)
-      get 'index'
+      get :index
       assigns[:corrections].should have_at_least(2).items
       assigns[:corrections].collect{|c| c.current}.should include(popeye)
     end
