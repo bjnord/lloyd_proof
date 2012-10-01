5b2414f3b99f8b35e567e4e3c53ec4a25a365d95
Brent J. Nordquist <brent@nordist.net>
2012-10-01 12:24:19 -0500

    New Android app

---
+++ android/src/net/nordist/lloydproof/LloydProof.java
@@ -0,0 +1,15 @@
+package net.nordist.lloydproof;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LloydProof extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
