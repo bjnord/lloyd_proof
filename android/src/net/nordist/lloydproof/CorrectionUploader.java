ae25de2f1f173e5e3f50411332e7074d6b6fbe0b
Brent J. Nordquist <brent@nordist.net>
2012-10-02 17:32:31 -0500

    Get corrections from store as JSON

--- android/src/net/nordist/lloydproof/CorrectionUploader.java
+++ android/src/net/nordist/lloydproof/CorrectionUploader.java
@@ -3,8 +3,9 @@ package net.nordist.lloydproof;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.AsyncTask;
-import android.os.SystemClock;
 import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class CorrectionUploader extends AsyncTask<Void, Void, Void>
 {
@@ -24,21 +25,37 @@ public class CorrectionUploader extends AsyncTask<Void, Void, Void>
     @Override
     protected void onPreExecute() {
         Log.d(TAG, "onPreExecute() fired");
+        // FIXME gray out Upload button
     }
 
     @Override
     protected Void doInBackground(Void... params) {
-        SystemClock.sleep(5000);  // FIXME also remove import
+        try {
+            JSONArray correction_json = store.getAllAsJsonArray();
+            Log.d(TAG, "upload JSON: " + correction_json.toString());
+            JSONArray status_json = this.uploadJson(correction_json);
+            Log.d(TAG, "status JSON: " + status_json.toString());
+            // FIXME delete from local DB based on status
+        } catch (JSONException jex) {
+            Log.e(TAG, "error constructing JSON: " + jex.getMessage());
+            this.cancel(true);
+        }
         return null;
     }
 
     @Override
     protected void onPostExecute(Void result) {
         Log.d(TAG, "onPostExecute() fired");
+        // FIXME reenable Upload button
     }
 
     @Override
     protected void onCancelled(Void result) {
         Log.d(TAG, "onCancelled() fired");
+        // FIXME reenable Upload button
+    }
+
+    protected JSONArray uploadJson(JSONArray corrections) {
+        return new JSONArray();
     }
 }
