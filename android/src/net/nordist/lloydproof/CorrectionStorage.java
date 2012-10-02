ec5abde85d1e556f893eaa592fd2ac053a6a613f
Brent J. Nordquist <brent@nordist.net>
2012-10-02 06:59:48 -0500

    First cut at local correction storage

---
+++ android/src/net/nordist/lloydproof/CorrectionStorage.java
@@ -0,0 +1,122 @@
+package net.nordist.lloydproof;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import java.util.Calendar;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CorrectionStorage extends SQLiteOpenHelper
+{
+    private final String TAG = this.getClass().getSimpleName();
+
+    private SQLiteDatabase writeDB = null;
+    private SQLiteDatabase readDB = null;
+
+    public static final String TABLE_NAME = "corrections";
+    private static final String DATABASE_NAME = "corrections.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_CREATE = "CREATE TABLE " + TABLE_NAME
+        + " (id integer PRIMARY KEY AUTOINCREMENT,"
+        + " current_text text NOT NULL DEFAULT '');";
+
+    public CorrectionStorage(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.w(TAG, "Creating database v" + DATABASE_VERSION);
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TAG, "Upgrading database from v" + oldVersion + " to v"
+            + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME + ";");
+        onCreate(db);
+    }
+
+    public int save(String current_text) {
+        ContentValues values = new ContentValues();
+        values.put("current_text", current_text);
+        this.openWriteDB();
+        return (int)writeDB.insert(TABLE_NAME, null, values);
+    }
+
+    public int count() {
+        this.openReadDB();
+        Cursor cursor = readDB.query(TABLE_NAME, new String[] {"COUNT(1)"},
+            null, null, null, null, null);
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            return cursor.getInt(0);
+        } else {
+            return 0;
+        }
+    }
+
+    private JSONObject getNextAsJsonObject(Cursor cursor) throws JSONException {
+        JSONObject jobject = new JSONObject();
+        jobject.put("sync_id", cursor.getInt(0));
+        jobject.put("current_text", cursor.getString(1));
+        return jobject;
+    }
+
+    public JSONArray getAllAsJsonArray() throws JSONException {
+        this.openReadDB();
+        Cursor cursor = readDB.query(TABLE_NAME,
+            new String[] {"id", "current_text"},
+            null, null, null, null, null);
+        JSONArray jarray = new JSONArray();
+        while (cursor.moveToNext()) {
+            jarray.put(this.getNextAsJsonObject(cursor));
+        }
+        return jarray;
+    }
+
+    // FIXME change to delete by JSON status array
+    public void deleteById(int id) {
+        String where = "id=?";
+        String[] wherevals = new String[] {Integer.toString(id)};
+        this.openWriteDB();
+        writeDB.delete(TABLE_NAME, where, wherevals);
+    }
+
+    public void close() {
+        if (writeDB != null) {
+            writeDB.close();
+            writeDB = null;
+        }
+        if (readDB != null) {
+            readDB.close();
+            readDB = null;
+        }
+    }
+
+    protected void finalize() throws Throwable {
+        try {
+            close();
+        } finally {
+            super.finalize();
+        }
+    }
+
+    private void openWriteDB() {
+        if (writeDB == null) {
+            writeDB = this.getWritableDatabase();
+        }
+    }
+
+    private void openReadDB() {
+        if (readDB == null) {
+            readDB = this.getReadableDatabase();
+        }
+    }
+}
--- android/src/net/nordist/lloydproof/LloydProof.java
+++ android/src/net/nordist/lloydproof/LloydProof.java
@@ -2,14 +2,47 @@ package net.nordist.lloydproof;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LloydProof extends Activity
 {
-    /** Called when the activity is first created. */
+    private final String TAG = this.getClass().getSimpleName();
+
+    private CorrectionStorage store;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        store = new CorrectionStorage(this);
         setContentView(R.layout.main);
+    }
+
+    public void saveCorrection(View view) {
+        EditText current_text = (EditText)findViewById(R.id.current_text);
+        // FIXME do nothing if EditText box is empty
+        int id = store.save(current_text.getText().toString());
+        if (id > 0) {
+            Log.d(TAG, "saved correction as id=" + id);
+            current_text.setText("", TextView.BufferType.EDITABLE);
+            // FIXME show success as flyover status
+        } else {
+            Log.e(TAG, "error saving correction");
+            // FIXME show error as flyover status
+        }
+        this.updateSyncStatus();
+    }
+
+    private void updateSyncStatus() {
+        TextView sync_status = (TextView)findViewById(R.id.sync_status);
+        int count = store.count();
+        // FIXME use string resources for i18n
+        if (count > 0) {
+            sync_status.setText(Integer.toString(count) + " corrections to upload");
+        } else {
+            sync_status.setText("All corrections uploaded");
+        }
     }
 }
