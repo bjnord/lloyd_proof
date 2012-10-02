e5ec351fcd41658456a9660d903b8ea1d5331395
Brent J. Nordquist <brent@nordist.net>
2012-10-02 12:24:49 -0500

    Use string/plurals resources

--- android/src/net/nordist/lloydproof/LloydProof.java
+++ android/src/net/nordist/lloydproof/LloydProof.java
@@ -2,6 +2,7 @@ package net.nordist.lloydproof;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,11 +16,13 @@ public class LloydProof extends Activity
 
     private CorrectionStorage store;
     private Context context;
+    private Resources resources;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = getApplicationContext();
+        resources = context.getResources();
         store = new CorrectionStorage(this);
         setContentView(R.layout.main);
     }
@@ -28,14 +31,13 @@ public class LloydProof extends Activity
         EditText current_text = (EditText)findViewById(R.id.current_text);
         // FIXME do nothing if EditText box is empty
         int id = store.save(current_text.getText().toString());
-        // FIXME use string resources for i18n
         if (id > 0) {
             Log.d(TAG, "saved correction as id=" + id);
             current_text.setText("", TextView.BufferType.EDITABLE);
-            this.showStatus("Correction saved.");
+            this.showStatus(resources.getString(R.string.correction_saved));
         } else {
             Log.e(TAG, "error saving correction");
-            this.showStatus("Error saving correction.");
+            this.showStatus(resources.getString(R.string.correction_save_error));
         }
         this.updateSyncStatus();
     }
@@ -47,11 +49,11 @@ public class LloydProof extends Activity
     private void updateSyncStatus() {
         TextView sync_status = (TextView)findViewById(R.id.sync_status);
         int count = store.count();
-        // FIXME use string resources for i18n
         if (count > 0) {
-            sync_status.setText(Integer.toString(count) + " corrections to upload");
+            String plural_count = resources.getQuantityString(R.plurals.corrections, count, count);
+            sync_status.setText(plural_count + " " + resources.getString(R.string.to_upload));
         } else {
-            sync_status.setText("All corrections uploaded");
+            sync_status.setText(resources.getString(R.string.all_uploaded));
         }
     }
 }
