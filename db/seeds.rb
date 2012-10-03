cdb1b9cc541136408f3456ea9ae98254626d4ff3
Brent J. Nordquist <brent@nordist.net>
2012-10-03 05:51:53 -0500

    Rename attribute to Correction.current_text
    One spot missed in 0d7db0d commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'factory_girl_rails'
 
 Correction.delete_all
-FactoryGirl.create(:correction, :current => 'Call me Ishmeal.')
-FactoryGirl.create(:correction, :current => 'It was a bright cold day in April, and the clocks were stricking thirteen.')
-FactoryGirl.create(:correction, :current => 'The sky above the port was the color of television, turned to a dead channel.')
+FactoryGirl.create(:correction, :current_text => 'Call me Ishmeal.')
+FactoryGirl.create(:correction, :current_text => 'It was a bright cold day in April, and the clocks were stricking thirteen.')
+FactoryGirl.create(:correction, :current_text => 'The sky above the port was the color of television, turned to a dead channel.')
