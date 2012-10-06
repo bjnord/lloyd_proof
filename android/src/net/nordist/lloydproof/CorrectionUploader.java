44d9a692bf066ac67ef7e863097d88cf25edce27
Brent J. Nordquist <brent@nordist.net>
2012-10-06 08:18:54 -0500

    RF: Remove unneeded import

--- android/src/net/nordist/lloydproof/CorrectionUploader.java
+++ android/src/net/nordist/lloydproof/CorrectionUploader.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
