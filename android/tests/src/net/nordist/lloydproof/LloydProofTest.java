f57d308359476309c8316af28a2d33ac361e5468
Brent J. Nordquist <brent@nordist.net>
2012-10-06 10:28:24 -0500

    Add copyright/license for Android code

--- android/tests/src/net/nordist/lloydproof/LloydProofTest.java
+++ android/tests/src/net/nordist/lloydproof/LloydProofTest.java
@@ -1,3 +1,6 @@
+// Copyright © 2012 by Brent J. Nordquist. Some Rights Reserved.
+// This work is licensed under http://creativecommons.org/licenses/by-sa/3.0/
+
 package net.nordist.lloydproof;
 
 import android.app.Instrumentation;
