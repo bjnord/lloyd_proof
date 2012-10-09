dc3fef7e8f04b763016c5bf7daa521405a83ef3d
Brent J. Nordquist <brent@nordist.net>
2012-10-09 16:59:44 -0500

    Working instrumented test for SettingsActivity

---
+++ android/tests/src/net/nordist/lloydproof/SettingsActivityTest.java
@@ -0,0 +1,80 @@
+// Copyright © 2012 by Brent J. Nordquist. Some Rights Reserved.
+// This work is licensed under http://creativecommons.org/licenses/by-sa/3.0/
+
+package net.nordist.lloydproof;
+
+import android.app.AlertDialog;
+import android.app.Instrumentation;
+import android.app.KeyguardManager.KeyguardLock;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.preference.EditTextPreference;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.widget.EditText;
+
+public class SettingsActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity>
+{
+    private static final String TAG = "SettingsActivityTest";
+    private SettingsActivity activity;
+    private KeyguardLock keyguardLock;
+
+    public SettingsActivityTest() {
+        super("net.nordist.lloydproof", SettingsActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        activity = getActivity();
+        KeyguardManager keyGuardManager = (KeyguardManager)activity.getApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
+        keyguardLock = keyGuardManager.newKeyguardLock("SettingsActivity");
+        keyguardLock.disableKeyguard();
+    }
+
+    /*
+     * The emulator seems to need a little time to settle at various points,
+     * otherwise tests fail.
+     */
+    private void settleWait() {
+        getInstrumentation().waitForIdleSync();
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+        }
+    }
+
+    public void testChangingServerURL() {
+        // FIXME use class constant for "pref_server_url"
+        final EditTextPreference serverURLPreference = (EditTextPreference)activity.findPreference("pref_server_url");
+        assertNotNull(serverURLPreference);
+        activity.runOnUiThread(
+            new Runnable() {
+                public void run() {
+                    // click "Server URL" to bring up the dialog
+                    // FIXME magic constant 0 won't work when we have more than 1
+                    final int SERVER_URL_POSITION = 0;
+                    activity.getPreferenceScreen().onItemClick(null, null, SERVER_URL_POSITION, 0);
+                    // focus EditText box
+                    final EditText serverURL = serverURLPreference.getEditText();
+                    assertNotNull(serverURL);
+                    serverURL.requestFocus();
+                }
+            }
+        );
+        settleWait();
+        // FIXME should be "http:" but I can't get the shift key to work
+        sendKeys("H T T P PERIOD SLASH SLASH 1 2 7 PERIOD 0 PERIOD 0 PERIOD 1 SLASH");
+        activity.runOnUiThread(
+            new Runnable() {
+                public void run() {
+                    // click "OK"
+                    AlertDialog dialog = (AlertDialog)serverURLPreference.getDialog();
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).performClick();
+                }
+            }
+        );
+        settleWait();
+        assertEquals("http.//127.0.0.1/", serverURLPreference.getText());
+    }
+}
