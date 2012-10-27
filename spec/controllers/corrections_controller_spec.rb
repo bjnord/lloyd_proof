a70a7ab39af43397ec7e61744dd266182787577d
Brent J. Nordquist <brent@nordist.net>
2012-10-27 07:15:23 -0500

    Final step toward simpler mock/subject/let spec
    this uses shoulda matchers

--- spec/controllers/corrections_controller_spec.rb
+++ spec/controllers/corrections_controller_spec.rb
@@ -7,61 +7,28 @@ describe CorrectionsController do
 
   describe '#index' do
     let(:corrections) { mock(Array) }
-    subject { get :index }
-
     before(:each) do
       Correction.should_receive(:all).and_return(corrections)
+      get :index
     end
+    subject { controller }
 
     it { should render_template(:index) }
+    it { should assign_to(:corrections).with(corrections) }
   end
 
-  describe 'upload action' do
+  describe '#upload' do
+    let(:sync_id) { 555 }
+    let(:correction) { mock(Hash) }
+    let(:status) { Hash[:sync_id => sync_id] }
     before(:each) do
-      @corrections = [
-        {'sync_id' => '101', 'current_text' => 'irregardless'},
-        {'sync_id' => '102', 'current_text' => 'equally as'},
-        {'sync_id' => '103', 'current_text' => 'pour over'}
-      ]
+      Correction.should_receive(:create_and_return_status).twice.with(correction).and_return(status)
+      post :upload, :corrections => [correction, correction], :format => :json
     end
+    subject { controller }
 
-    it 'returns HTTP success' do
-      post :upload, :corrections => @corrections, :format => :json
-      response.should be_success
-    end
-
-    it 'accepts valid corrections' do
-      post :upload, :corrections => @corrections, :format => :json
-      JSON.parse(response.body)['upload_status'].each do |status|
-        status['status'].should == 'ok'
-      end
-    end
-
-    it 'selectively rejects invalid corrections' do
-      @corrections[1]['current_text'] = ''
-      bad_sync_id = @corrections[1]['sync_id']
-      post :upload, :corrections => @corrections, :format => :json
-      JSON.parse(response.body)['upload_status'].each do |status|
-        if status['sync_id'] == bad_sync_id
-          status['status'].should == 'error'
-        else
-          status['status'].should == 'ok'
-        end
-      end
-    end
-
-    it 'selectively rejects mass assignment attempts' do
-      @corrections[2]['updated_at'] = 'dawn'
-      bad_sync_id = @corrections[2]['sync_id']
-      post :upload, :corrections => @corrections, :format => :json
-      JSON.parse(response.body)['upload_status'].each do |status|
-        if status['sync_id'] == bad_sync_id
-          status['status'].should == 'error'
-        else
-          status['status'].should == 'ok'
-        end
-      end
-    end
+    it { should respond_with(:success) }
+    it { should assign_to(:statuses).with([status, status]) }
   end
 
   # DEPRECATED
