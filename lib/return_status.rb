59ec7dbb54ebe144d59f6fa6f1dcfe6ac9c155d8
Brent J. Nordquist <brent@nordist.net>
2012-10-27 12:11:35 -0500

    RF: Separate formatting from call/error handling

--- lib/return_status.rb
+++ lib/return_status.rb
@@ -3,6 +3,18 @@
 # License::    {CC Attribution-ShareAlike 3.0 Unported}[http://creativecommons.org/licenses/by-sa/3.0/]
 
 module ReturnStatus
+  class StatusFormatter
+    attr_reader :status
+
+    def initialize(sync_id)
+      @status = {:sync_id => sync_id, :status => :ok}
+    end
+
+    def errors=(errors)
+      @status.merge!({:status => :error, :errors => errors})
+    end
+  end
+
   #--
   # <http://stackoverflow.com/questions/3357712/static-methods-in-ruby-modules>
   def self.included(o)
@@ -10,20 +22,21 @@ module ReturnStatus
   end
 
   def save_and_return_status(sync_id)
-    return {:sync_id => sync_id, :status => :ok} if self.save
-    return {:sync_id => sync_id, :status => :error, :errors => self.errors.full_messages}
+    formatter = StatusFormatter.new(sync_id)
+    formatter.errors = self.errors.full_messages unless self.save
+    formatter.status
   end
 
   module ClassMethods
     def create_and_return_status(attr)
-      sync_id = attr.delete(:sync_id)
+      formatter = StatusFormatter.new(attr.delete(:sync_id))
       begin
         corr = Correction.create(attr)
-        return {:sync_id => sync_id, :status => :ok} if corr.persisted?
-        return {:sync_id => sync_id, :status => :error, :errors => corr.errors.full_messages}
+        formatter.errors = corr.errors.full_messages unless corr.persisted?
       rescue ActiveModel::MassAssignmentSecurity::Error => e
-        return {:sync_id => sync_id, :status => :error, :errors => [e.message]}
+        formatter.errors = [e.message]
       end
+      formatter.status
     end
   end
 end
